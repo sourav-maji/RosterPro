144f1488fcf9aa2e8cc5d92f775d9971fd759ba4
sourav-maji <s722143@gmail.com>
2026-01-19 18:25:05 +0530

    Writting the server file

---
+++ server.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import app from "./src/app.js";
+import connect from "./src/config/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
+
+(async () => {
+  try {
+    await connect();
+    app.listen(PORT, () => {
+      console.log(`RosterPro running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+})();
